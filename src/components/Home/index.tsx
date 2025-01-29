aa3ab0a0f583d904d7833cf0927f2040f816edc6
Marcos Melo <marcosmelo012@gmail.com>
2025-01-29 07:56:50 -0300

    My first boilerplate

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Text } from './styles';
+
+export default function Home() {
+  return (
+    <Container>
+      <Text>
+        Boilerplate Expo With Styled-Components! ;)
+      </Text>
+    </Container>
+  );
+}
---
+++ src/constants/StyledText.tsx
@@ -0,0 +1,5 @@
+import { Text } from 'react-native';
+
+export function MonoText() {
+  return <Text style={[{ fontFamily: 'SpaceMono' }]} />;
+}
