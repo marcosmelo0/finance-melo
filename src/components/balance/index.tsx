9262c6f03e867a7b0adc75ef4040eeafa104f789
Marcos Melo <marcosmelo012@gmail.com>
2025-02-17 16:05:37 -0300

    feat: enhance balance component layout and add toggle for balance visibility

--- src/components/balance/index.tsx
+++ src/components/balance/index.tsx
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from "react";
 import { DivBalance } from "./styles";
 import { Text } from "@/styles/container/style";
 import { useAuth } from "@/contexts/AuthContext";
+import colors from "@/constants/colors";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { View } from "react-native";
 
 export default function Balance() {
     const { user } = useAuth();
     const [loading, setLoading] = useState(true);
+    const [showBalance, setShowBalance] = useState(true)
 
     useEffect(() => {
         if (user) {
@@ -17,8 +21,16 @@ export default function Balance() {
 
     return (
         <DivBalance>
-            <Text size={16} fontWeight="bold">Saldo: </Text>
-            <Text size={16} fontWeight="bold">{formattedBalance}</Text>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: '100%', alignItems: 'center' }}>
+                <Text color={colors.zinc} size={16} fontWeight="bold">Saldo disponível: </Text>
+                <Text style={{ alignText: 'center' }} color={colors.zinc} size={showBalance ? 16 : 22} fontWeight="bold">{showBalance ? formattedBalance : '*******'}</Text>
+                <Ionicons
+                    name={showBalance ? "eye-off-outline" : "eye"}
+                    size={24}
+                    color="black"
+                    onPress={() => setShowBalance(!showBalance)}
+                />
+            </View>
         </DivBalance>
     )
 }
--- src/components/balance/styles.tsx
+++ src/components/balance/styles.tsx
@@ -5,4 +5,10 @@ export const DivBalance = styled.View`
     flex-direction: row;
     width: 100%;
     margin-top: 15px;
+    height: 50px;
+    background-color: aliceblue;
+    padding: 15px;
+    margin-bottom: 15px;
+    border-radius: 10px;
+    justify-content: center;
 `;
